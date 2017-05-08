49fd182344245e61c7208f85ad8963f0d2473d6a
Markus <mararon@chalmers.se>
2017-05-08 12:53:50 +0200

    update

--- src/PBKDF2.hs
+++ src/PBKDF2.hs
@@ -21,10 +21,8 @@ import Debug.Trace
 import Test.QuickCheck
 import qualified Data.HMAC as HMAC (hmac_sha1)
 
----------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- * PBKDF2.
---
--- Pseudocode specification for PBKDF2:
 --
 --     1. If dkLen > (2^32 - 1) * hLen, output "derived key too long" and
 --        stop.
@@ -66,6 +64,8 @@ import qualified Data.HMAC as HMAC (hmac_sha1)
 --
 --     5. Output the derived key DK.
 --
+--------------------------------------------------------------------------------
+
 type Password = [Word8] -- password, an octet string.
 type Salt     = [Word8] -- salt, an octet string.
 type Hash     = [Word8] -- derived key, a dkLen-octet string.
@@ -96,8 +96,43 @@ int i | length o > 4 = error "size of INT(i) > four-octet"
       | otherwise    = o
   where o = unroll i
 
----------------------------------------------------------------------
--- ** HMAC-SHA1.
+--------------------------------------------------------------------------------
+-- * HMAC-SHA1.
+--
+-- The definition of HMAC requires a cryptographic hash function, for which we
+-- will use SHA1 and we denote by H, and a secret key K. We denote by B the
+-- byte-length of blocks (B=64 for SHA1), and by L the byte-length of hash
+-- outputs (L=20 for SHA-1). The authentication key K can be of any length up
+-- to B. Applications that use keys longer than B bytes will first hash the key
+-- using H and then use the resultant L byte string as the actual key to HMAC.
+-- In any case the minimal recommended length for K is L bytes.
+--
+-- We define two fixed and different strings ipad and opad as follows
+-- (the 'i' and 'o' are mnemonics for inner and outer):
+--
+--     ipad = the byte 0x36 repeated B times
+--     opad = the byte 0x5C repeated B times.
+--
+-- To compute HMAC over the data `text' we perform
+--
+--     H(K XOR opad, H(K XOR ipad, text))
+--
+-- Namely,
+--
+--  (1) append zeros to the end of K to create a B byte string
+--      (e.g., if K is of length 20 bytes and B=64, then K will be
+--       appended with 44 zero bytes 0x00)
+--  (2) XOR (bitwise exclusive-OR) the B byte string computed in step
+--      (1) with ipad
+--  (3) append the stream of data 'text' to the B byte string resulting
+--      from step (2)
+--  (4) apply H to the stream generated in step (3)
+--  (5) XOR (bitwise exclusive-OR) the B byte string computed in
+--      step (1) with opad
+--  (6) append the H result from step (4) to the B byte string
+--      resulting from step (5)
+--  (7) apply H to the stream generated in step (6) and output
+--      the result
 --
 -- Pseudocode specification for HMAC-SHA1:
 --
@@ -114,29 +149,30 @@ int i | length o > 4 = error "size of INT(i) > four-octet"
 --    return hash(o_key_pad ∥ hash(i_key_pad ∥ message))
 --  }
 --
--- Where
---   * '∥' is concatenation
---   * '⊕' is exclusive or (XOR)
---   * '*' is repetition.
---   * 'blocksize' is that of the underlying hash function
---      (64 for SHA1)
---      
+-- Where '*' denotes repetition.
+--------------------------------------------------------------------------------
+
 hmac :: [Word8] -- message, an octet string.
      -> [Word8] -- encryption key, an octet string.
      -> [Word8] -- encoded message, an octet string.
-hmac msg ikey =
-  let key = case compare (length ikey) blocksize of
-        GT -> hash ikey
-        LT -> ikey ++ (0x00 `repeat` (blocksize - length ikey))
-        EQ -> ikey
-      o_key_pad = (0x5c `repeat` blocksize) `mxor` key
-      i_key_pad = (0x36 `repeat` blocksize) `mxor` key
+hmac msg key =
+  let sized_key = hmac_fitted_key key
+      o_key_pad = (0x5c `mrepeat` hmac_blocksize) `mxor` sized_key
+      i_key_pad = (0x36 `mrepeat` hmac_blocksize) `mxor` sized_key
   in
   hash(o_key_pad ++ hash(i_key_pad ++ msg))
-  where
-    repeat    = flip replicate :: Word8 -> Int -> [Word8]    
-    blocksize = 64 :: Int
 
+hmac_fitted_key :: [Word8] -> [Word8]
+hmac_fitted_key key
+  | length key > hmac_blocksize = hmac_fitted_key (hash key)
+  | otherwise  = key ++ (0x00 `mrepeat` (hmac_blocksize - length key))
+
+hmac_blocksize :: Int
+hmac_blocksize = 64
+
+hmac_hashsize :: Int
+hmac_hashsize = 20
+  
 -- length in octets of pseudorandom function output, 
 -- a positive integer. (SHA1 outputs 160 bits).
 hLen :: Integer
@@ -147,14 +183,26 @@ hLen = 160 `div` 8
 hash :: [Word8] -> [Word8]
 hash = unroll . SHA1.toInteger . SHA1.hash
 
----------------------------------------------------------------------
--- ** Helpers.
+----------------------------------------
+
+prop_hmac_key_len :: [Word8] -> Property
+prop_hmac_key_len key = len >= hmac_hashsize .&&. len <= hmac_blocksize
+  where len = length $ hmac_fitted_key key
+
+--------------------------------------------------------------------------------
+-- ** Operations from above specifications.
 
 mdiv :: Integer -> Integer -> Integer
 mdiv a b = ceiling ((fromIntegral a) / (fromIntegral b))
 
 mxor :: [Word8] -> [Word8] -> [Word8]
 mxor = zipWith (xor)
+
+mrepeat :: Word8 -> Int -> [Word8]
+mrepeat = flip replicate
+
+--------------------------------------------------------------------------------
+-- ** Conversion from words to integers.
 
 unroll :: Integer -> [Word8]
 unroll = unfoldr stepR
@@ -167,38 +215,22 @@ roll = foldr stepL 0
   where
     stepL b a = a `shiftL` 8 .|. fromIntegral b
 
-----------------------------------------------------------------------
--- Older stuff.
-
-unpackWord160 :: SHA1.Word160 -> [Word8]
-unpackWord160 (SHA1.Word160 a b c d e) = concatMap octets [a, b, c, d, e] 
-
-octets :: Word32 -> [Word8]
-octets w = 
-    [ fromIntegral (w `shiftR` 24)
-    , fromIntegral (w `shiftR` 16)
-    , fromIntegral (w `shiftR` 8)
-    , fromIntegral w
-    ]
-
-----------------------------------------------------------------------
--- ** Testing & QuickCheck.
-
--- DK = PBKDF2(HMAC−SHA1, passphrase, ssid, 4096, 256) in WPA2.
-
-wpa2 pswd salt = pbkdf2 pswd salt 4096 256
-
--- HMAC_SHA1("key", "The quick brown fox jumps over the lazy dog") =
---   0xde7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9
-
--- For some reason, my hmac function, the one imported from a library,
--- and the wiki's example, all produce different hashes.
-msg = S.encode "The quick brown fox jumps over the lazy dog"
-key = S.encode "key"             
-res = 0xde7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9 :: Integer
+----------------------------------------
 
 prop_octets_id :: Integer -> Property
 prop_octets_id i = i >= 0 ==> roll (unroll i) === i
 
----------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- ** Testing & QuickCheck.
+
+-- In WPA2: DK = PBKDF2(HMAC−SHA1, passphrase, ssid, 4096, 256).
+wpa2 pswd salt = pbkdf2 pswd salt 4096 256
+
+-- HMAC_SHA1("key", "The quick brown fox jumps over the lazy dog") =
+--   0xde7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9
+msg = S.encode "The quick brown fox jumps over the lazy dog"
+key = S.encode "key"             
+res = 0xde7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9 :: Integer
+
+--------------------------------------------------------------------------------
 -- the end.
