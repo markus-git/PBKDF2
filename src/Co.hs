0d69966a683a51042395820dd034a16a0b6d5c52
Markus <mararon@chalmers.se>
2017-05-10 11:30:36 +0200

    co-design start

---
+++ src/Co.hs
@@ -0,0 +1,6 @@
+module Co where
+
+import qualified Language.VHDL              as V
+import qualified Language.Embedded.Hardware as H
+
+--------------------------------------------------------------------------------
